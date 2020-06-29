61d28037cca6983f31d257c0b037ee2d0d42f9b8
taiwocoker <taiwofcoker@gmail.com>
2020-06-29 15:30:39 +0100

    add model and validation and association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates_presence_of :username
   validates_uniqueness_of :username, :case_sensitive => false
   validates :username, :length => {:in => 6..30}
