c1da8bed8314b44bcb376c3977c3390262224753
Mupa1 <mupasmail@gmail.com>
2020-06-29 16:00:08 +0200

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates_presence_of :username
+  validates_uniqueness_of :username, :case_sensitive => false
+  validates :username, :length => {:in => 6..30}
 end
